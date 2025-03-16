58d3ccfdfa3f7b914214beae9844295afd2b462a
POTATOPROGAMMER <codercode1010@gmail.com>
2025-03-16 13:35:38 +0400

    commit1

---
+++ activity1.py
@@ -0,0 +1,7 @@
+ch=input("enter the character : ")
+if ch.isalpha():
+    print("this character is an alphabet")
+elif  ch.isdigit():
+   print("this is a digit")
+else:
+    print("this character is not an  alphabet")
